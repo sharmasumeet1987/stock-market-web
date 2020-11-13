d3056617e41b29f6d471a571817f36d9b9e5da3a
Sumeet Sharma <sharmasumeet1987@gmail.com>
2020-11-13 00:58:12 -0500

    mat button added

--- src/app/companies-dashboard/companies-dashboard.module.ts
+++ src/app/companies-dashboard/companies-dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CompaniesDashboardRoutingModule } from './companies-dashboard-routing.module';
 import { CompaniesDashboardComponent } from './companies-dashboard/companies-dashboard.component';
+import { MaterialModule } from '../material/material.module';
 
 @NgModule({
   declarations: [CompaniesDashboardComponent],
-  imports: [CommonModule, CompaniesDashboardRoutingModule],
+  imports: [CommonModule, CompaniesDashboardRoutingModule, MaterialModule],
 })
 export class CompaniesDashboardModule {}
